b06517c4d6efc963ca2fa1e221688975ee35e6fa
Bharathraj E <bharathraj@instrive.in>
2025-04-07 13:45:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecom_ui_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ecom_ui_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ecom_ui_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
